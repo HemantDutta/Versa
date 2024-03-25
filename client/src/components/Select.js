496f996160d9e30eee6eb0df1fd2cc4c3e6b6092
HemantDutta <hemantnorddutta2@gmail.com>
2024-03-25 22:11:32 +0530

    Create Select Component

---
+++ client/src/components/Select.js
@@ -0,0 +1,16 @@
+export const Select = ({name, setState, children}) => {
+    return (
+        <div className="select-wrapper rounded overflow-hidden">
+            <div className="select-border"/>
+            <select name={name} id={name} className="bg-black text-white p-3 rounded"
+                    onChange={(e) => {
+                        name === "fonts" ?
+                            setState(JSON.parse(e.target.value))
+                            : setState(e.target.value)
+                    }}
+            >
+                {children}
+            </select>
+        </div>
+    )
+}
--- client/src/pages/Editor.js
+++ client/src/pages/Editor.js
@@ -1,7 +1,12 @@
 import axios from "axios";
 import {useEffect, useRef, useState} from "react";
+import '../styles/Editor.css';
+import {Select} from "../components/Select";
 
 export const Editor = () => {
+
+    //Temp Static
+    const styles = ["Dracula", "PoopCard", "ChipStick", "DickWaggler", "MakeBread", "FuckBitches"];
 
     //Refs
     const first = useRef(true);
@@ -9,6 +14,7 @@ export const Editor = () => {
     //States
     const [fonts, setFonts] = useState([]);
     const [selectedFont, setSelectedFont] = useState({});
+    const [selectedStyle, setSelectedStyle] = useState("");
 
     //Fetch Google Fonts
     function fetchGoogleFonts() {
@@ -25,7 +31,6 @@ export const Editor = () => {
     function setupFontFace() {
         try {
             const fontUrl = selectedFont.files?.regular;
-            const fontName = selectedFont.family.replace(/\s+/g, '');
             const fontFaceRule = `@font-face {
           font-family: '${selectedFont.family}';
           src: url('${fontUrl}') format('truetype');
@@ -57,53 +62,57 @@ export const Editor = () => {
 
     return (
         <>
-            <header className="bg-dark p-3 flex gap-x-5 items-center justify-between">
-                <div className="left flex items-center gap-x-5">
-                    <span className="brand user-select-none cursor-pointer text-white font-bold text-2xl">GracePDF</span>
-                    <div className="tools flex items-center gap-x-5 flex-wrap">
-                        <span title="Insert Bold Text" className="cursor-pointer user-select-none p-2 aspect-square text-white font-bold text-xl hover:bg-gray-700 rounded active:bg-white active:text-black">B</span>
-                        <span title="Insert Italic Text" className="cursor-pointer user-select-none p-2 aspect-square text-white font-bold text-xl hover:bg-gray-700 rounded active:bg-white italic active:text-black">I</span>
-                        <del title="Insert Strike Through Text" className="cursor-pointer user-select-none p-2 aspect-square text-white font-bold text-xl hover:bg-gray-700 rounded active:bg-white active:text-black">ST</del>
-                        <span title="Insert Blockquote" className="cursor-pointer user-select-none p-2 aspect-square text-white font-bold text-xl hover:bg-gray-700 rounded active:bg-white active:text-black">>_</span>
-                        <span title="Insert Unordered List" className="cursor-pointer user-select-none p-2 aspect-square text-white font-bold text-xl hover:bg-gray-700 rounded active:bg-white active:text-black"><i className="fa-solid fa-list-ul"/></span>
-                        <span title="Insert Ordered List" className="cursor-pointer user-select-none p-2 aspect-square text-white font-bold text-xl hover:bg-gray-700 rounded active:bg-white active:text-black"><i className="fa-solid fa-list-ol"/></span>
-                        <span title="Insert Code Block" className="cursor-pointer user-select-none p-2 aspect-square text-white font-bold text-xl hover:bg-gray-700 rounded active:bg-white active:text-black"> {"</>"} </span>
-                    </div>
-                </div>
-                <div className="right flex items-center gap-x-5">
-                    <div className="font-options flex flex-col">
-                        <label htmlFor="fonts" className="text-white">Choose Font</label>
-                        <select name="fonts" id="fonts" className="bg-black text-white p-3"
-                                onChange={(e) => {
-                                    setSelectedFont(JSON.parse(e.target.value))
-                                }}
-                        >
-                            {
-                                fonts.map((value, index) => {
-                                    return (
-                                        <option key={index} value={JSON.stringify(value)} className="flex flex-col gap-2">
-                                            {value.family}
-                                        </option>
-                                    )
-                                })
-                            }
-                        </select>
-                    </div>
-                    <div className="operations">
-                        <div className="style flex flex-col">
-                            <label htmlFor="style" className="text-white">Choose Style</label>
-                            <select name="style" id="style" className="bg-black text-white p-3">
-
-                            </select>
+            <section className="editor">
+                <header className="bg-dark p-3 flex gap-x-5 items-center justify-between">
+                    <div className="left flex items-center gap-x-5">
+                        <span className="brand user-select-none cursor-pointer text-white font-bold text-2xl">GracePDF</span>
+                        <div className="tools flex items-center gap-x-5 flex-wrap">
+                            <span title="Insert Bold Text" className="cursor-pointer user-select-none p-2 aspect-square text-white font-bold text-xl hover:bg-gray-700 rounded active:bg-white active:text-black">B</span>
+                            <span title="Insert Italic Text" className="cursor-pointer user-select-none p-2 aspect-square text-white font-bold text-xl hover:bg-gray-700 rounded active:bg-white italic active:text-black">I</span>
+                            <del title="Insert Strike Through Text" className="cursor-pointer user-select-none p-2 aspect-square text-white font-bold text-xl hover:bg-gray-700 rounded active:bg-white active:text-black">ST</del>
+                            <span title="Insert Blockquote" className="cursor-pointer user-select-none p-2 aspect-square text-white font-bold text-xl hover:bg-gray-700 rounded active:bg-white active:text-black">>_</span>
+                            <span title="Insert Unordered List" className="cursor-pointer user-select-none p-2 aspect-square text-white font-bold text-xl hover:bg-gray-700 rounded active:bg-white active:text-black"><i className="fa-solid fa-list-ul"/></span>
+                            <span title="Insert Ordered List" className="cursor-pointer user-select-none p-2 aspect-square text-white font-bold text-xl hover:bg-gray-700 rounded active:bg-white active:text-black"><i className="fa-solid fa-list-ol"/></span>
+                            <span title="Insert Code Block" className="cursor-pointer user-select-none p-2 aspect-square text-white font-bold text-xl hover:bg-gray-700 rounded active:bg-white active:text-black"> {"</>"} </span>
                         </div>
                     </div>
-                </div>
-            </header>
-            {/*  Editor  */}
-            {/*  Editor End  */}
-            {/*  Preview  */}
-            <span style={{fontFamily: selectedFont.family}}>Hello</span>
-            {/*  Preview End  */}
+                    <div className="right flex items-center gap-x-5">
+                        <div className="font-options flex flex-col">
+                            <label htmlFor="fonts" className="text-white">Choose Font</label>
+                            <Select name="fonts" setState={setSelectedFont}>
+                                {
+                                    fonts.map((value, index) => {
+                                        return (
+                                            <option key={index} value={JSON.stringify(value)}>
+                                                {value.family}
+                                            </option>
+                                        )
+                                    })
+                                }
+                            </Select>
+                        </div>
+                        <div className="operations">
+                            <div className="style flex flex-col">
+                                <label htmlFor="style" className="text-white">Choose Style</label>
+                                <Select name="style" setState={setSelectedStyle}>
+                                    {
+                                        styles.map((value, index) => {
+                                            return (
+                                                <option value={value} key={index}>{value}</option>
+                                            )
+                                        })
+                                    }
+                                </Select>
+                            </div>
+                        </div>
+                    </div>
+                </header>
+                {/*  Editor  */}
+                {/*  Editor End  */}
+                {/*  Preview  */}
+                <span style={{fontFamily: selectedFont.family}}>Hello</span>
+                {/*  Preview End  */}
+            </section>
         </>
     )
 }
