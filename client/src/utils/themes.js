d8b58aadf53ad0dc0a1db7067e551f67307b9bd1
HemantDutta <hemantnorddutta2@gmail.com>
2024-04-09 12:51:16 +0530

    Add Pearl Theme

--- client/src/utils/themes.js
+++ client/src/utils/themes.js
@@ -9,6 +9,7 @@ export const themeColors = {
     "Lime Bar": "#141414",
     "Honor": "#f7f5f0",
     "MoonShine": "#151226",
+    "Pearl": "#0e142c"
 }
 
 export const themes = {
@@ -1279,6 +1280,162 @@ export const themes = {
         "    display: block;\n" +
         "    margin: 16px 0 0;\n" +
         "    box-shadow: 0px 10px 0 0 #7b71ff;\n" +
+        "}\n" +
+        "\n" +
+        "#styledHtml p {\n" +
+        "    font-size: 20px;\n" +
+        "    margin: 18px 0 10px;\n" +
+        "}",
+    "Pearl": "#styledHtml {\n" +
+        "    width: 100%;\n" +
+        "    height: 100%;\n" +
+        "    background-color: #0e142c;\n" +
+        "    color: #fdfdfe;\n" +
+        "}\n" +
+        "\n" +
+        "#styledHtml h1, #styledHtml h2, #styledHtml h3, #styledHtml h4, #styledHtml h5, #styledHtml h6 {\n" +
+        "    margin: 32px 0 0;\n" +
+        "    font-weight: bold;\n" +
+        "    text-wrap: normal;\n" +
+        "    color: #fdfdfe;\n" +
+        "}\n" +
+        "\n" +
+        "#styledHtml h1 {\n" +
+        "    position: relative;\n" +
+        "    font-size: 42px;\n" +
+        "    margin: 50px 0 32px;\n" +
+        "    z-index: 100;\n" +
+        "    width: max-content;\n" +
+        "    max-width: 100%;\n" +
+        "    background: linear-gradient(to right, #9c92ff, #ffa9b3, #faaef9);\n" +
+        "    -webkit-text-fill-color: transparent;\n" +
+        "    -webkit-background-clip: text;\n" +
+        "}\n" +
+        "\n" +
+        "#styledHtml h1:after {\n" +
+        "    position: absolute;\n" +
+        "    content: \"\";\n" +
+        "    left: 0;\n" +
+        "    bottom: -15px;\n" +
+        "    width: 80%;\n" +
+        "    height: 2px;\n" +
+        "    background: linear-gradient(to right, #9c92ff, #ffa9b3, #faaef9, #ffffff);\n" +
+        "}\n" +
+        "\n" +
+        "#styledHtml h2 {\n" +
+        "    font-size: 24px;\n" +
+        "    margin: 42px 0 0;\n" +
+        "}\n" +
+        "\n" +
+        "#styledHtml h3 {\n" +
+        "    font-size: 22px;\n" +
+        "}\n" +
+        "\n" +
+        "#styledHtml h4 {\n" +
+        "    font-size: 20px;\n" +
+        "}\n" +
+        "\n" +
+        "#styledHtml h5 {\n" +
+        "    font-size: 18px;\n" +
+        "}\n" +
+        "\n" +
+        "#styledHtml h6 {\n" +
+        "    font-size: 16px;\n" +
+        "}\n" +
+        "\n" +
+        "#styledHtml ul {\n" +
+        "    list-style: square;\n" +
+        "}\n" +
+        "\n" +
+        "#styledHtml ol {\n" +
+        "    list-style: decimal;\n" +
+        "}\n" +
+        "\n" +
+        "#styledHtml ul, #styledHtml ol {\n" +
+        "    padding-left: 32px;\n" +
+        "    display: flex;\n" +
+        "    flex-direction: column;\n" +
+        "    gap: 10px;\n" +
+        "    margin: 16px 0 0;\n" +
+        "}\n" +
+        "\n" +
+        "#styledHtml .blockquote {\n" +
+        "    margin: 32px 0;\n" +
+        "    border-left: 4px solid #ffa9b3;\n" +
+        "    padding-left: 23px;\n" +
+        "    font-size: 20px;\n" +
+        "    padding-top: 5px;\n" +
+        "    padding-bottom: 5px;\n" +
+        "    color: #ffffff;\n" +
+        "    background: linear-gradient(to right, rgba(156, 146, 255, 0.3), rgba(255, 169, 179, 0.3), rgba(250, 174, 249, 0.3));\n" +
+        "}\n" +
+        "\n" +
+        "#styledHtml pre {\n" +
+        "    margin: 56px 0 0;\n" +
+        "    padding: 32px;\n" +
+        "    border: 2px solid #ffa9b3;\n" +
+        "    background:\n" +
+        "            radial-gradient(35.36% 35.36% at 100% 25%,#0000 66%,#3e282a 68% 70%,#0000 72%) 16px 16px/calc(2*16px) calc(2*16px),\n" +
+        "            radial-gradient(35.36% 35.36% at 0    75%,#0000 66%,#3e282a 68% 70%,#0000 72%) 16px 16px/calc(2*16px) calc(2*16px),\n" +
+        "            radial-gradient(35.36% 35.36% at 100% 25%,#0000 66%,#3e282a 68% 70%,#0000 72%) 0 0/calc(2*16px) calc(2*16px),\n" +
+        "            radial-gradient(35.36% 35.36% at 0    75%,#0000 66%,#3e282a 68% 70%,#0000 72%) 0 0/calc(2*16px) calc(2*16px),\n" +
+        "            repeating-conic-gradient(#0e142c 0 25%,#0000 0 50%) 0 0/calc(2*16px) calc(2*16px),\n" +
+        "            radial-gradient(#0000 66%,#3e282a 68% 70%,#0000 72%) 0 calc(16px/2)/16px 16px\n" +
+        "            #0e142c;\n" +
+        "    font-family: monospace;\n" +
+        "}\n" +
+        "\n" +
+        "#styledHtml pre code {\n" +
+        "    padding: 0;\n" +
+        "    background-color: transparent;\n" +
+        "    color: #f7f5f0;\n" +
+        "}\n" +
+        "\n" +
+        "#styledHtml code {\n" +
+        "    padding: 0 5px;\n" +
+        "    background-color: #faaef9;\n" +
+        "    font-family: monospace;\n" +
+        "    color: #0e142c;\n" +
+        "}\n" +
+        "\n" +
+        "#styledHtml strong {\n" +
+        "    font-weight: bold;\n" +
+        "}\n" +
+        "\n" +
+        "#styledHtml table {\n" +
+        "    margin: 32px 0 0;\n" +
+        "}\n" +
+        "\n" +
+        "#styledHtml table tr:first-child td {\n" +
+        "    color: #fdfdfe;\n" +
+        "    font-weight: bold;\n" +
+        "    background-color: #ffa9b3;\n" +
+        "}\n" +
+        "\n" +
+        "#styledHtml table td {\n" +
+        "    padding: 16px;\n" +
+        "    border: 1px solid #fdfdfe;\n" +
+        "}\n" +
+        "\n" +
+        "#styledHtml em {\n" +
+        "    font-style: italic;\n" +
+        "}\n" +
+        "\n" +
+        "#styledHtml a {\n" +
+        "    color: #fdfdfe;\n" +
+        "    text-decoration: underline;\n" +
+        "    -webkit-tap-highlight-color: transparent;\n" +
+        "}\n" +
+        "\n" +
+        "#styledHtml > a {\n" +
+        "    display: inline-block;\n" +
+        "    margin: 32px 0;\n" +
+        "}\n" +
+        "\n" +
+        "#styledHtml img {\n" +
+        "    display: block;\n" +
+        "    margin: 16px 0 0;\n" +
+        "    box-shadow: 0 0 15px #faaef9;\n" +
         "}\n" +
         "\n" +
         "#styledHtml p {\n" +
