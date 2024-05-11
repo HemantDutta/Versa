e0bae0bfdfe019db6c8518a6916bd43ec8500209
Hemant Dutta <hemantnorddutta2@gmail.com>
2024-05-12 03:46:53 +0530

    Fix Desc Loader Issue

--- client/src/components/KeyPoint.js
+++ client/src/components/KeyPoint.js
@@ -1,18 +1,17 @@
 import { useRef } from "react";
 import "../styles/KeyPoint.css";
 
-export const KeyPoint = ({data}) => {
+export const KeyPoint = ({ data }) => {
 
     //Full Description Loader (I am making this pointless shit at 2am, what am I doing with my life?)
     function fullDescLoader() {
         let fullDesc = document.getElementById(`full-desc-${data.serial}`)
-        console.log(data.serial)
-        if(fullDesc.classList.contains("active")) {
-            fullDesc.classList.remove("active");
-        }
-        else {
-            fullDesc.classList.add("active");
-        }
+        fullDesc.classList.add("active");
+    }
+
+    function closefullDesc() {
+        let fullDesc = document.getElementById(`full-desc-${data.serial}`)
+        fullDesc.classList.remove("active");
     }
 
     return (
@@ -23,9 +22,9 @@ export const KeyPoint = ({data}) => {
                 <span className="description text-gray-600 text-sm">{data.desc}</span>
                 <button type="button" onClick={fullDescLoader} className="w-max load-big">Know More</button>
             </div>
-            <div id={`full-desc-${data.serial}`} onMouseLeave={fullDescLoader} className="full-desc py-2 px-5 absolute h-full w-full top-0 left-0 flex flex-col gap-5">
+            <div id={`full-desc-${data.serial}`} onMouseLeave={closefullDesc} className="full-desc py-2 px-5 absolute h-full w-full top-0 left-0 flex flex-col gap-5">
                 <span>{data.bigDesc}</span>
-                <span className="sm:hidden block underline underline-offset-2" onClick={fullDescLoader}>Close</span>
+                <span className="sm:hidden block underline underline-offset-2" onClick={closefullDesc}>Close</span>
             </div>
         </div>
     )
